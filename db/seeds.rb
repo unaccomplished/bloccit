9bbdacd943d80647e0aefcac1992fa54e150a5c8
Jocelyn Hsu <hsu.joce@gmail.com>
2016-05-15 03:02:06 +0000

    Assignment 30 completed - Create unique post and comment

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,6 +17,21 @@ posts = Post.all
     )
 end
 
+# Create post with unique title and body
+Post.find_or_create_by(
+        title: "This is a unique title",
+        body: "This is a unique body. This should only show up once."
+)
+
+# Create unique comment for unique post above
+Comment.find_or_create_by(
+        post: Post.find_or_create_by(
+            title: "This is a unique title",
+            body: "This is a unique body. This should only show up once."
+        ),
+        body: "This is a unique comment. This should only show up once."
+)
+
 puts "Seed finished"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
