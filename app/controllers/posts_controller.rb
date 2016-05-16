0828842881868529db20fc52667fee761af68acd
Jocelyn Hsu <hsu.joce@gmail.com>
2016-05-16 03:04:57 +0000

    Assignment 31 completed - SPAM censorship

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   def index
+    spam
     @posts = Post.all
   end
 
@@ -11,4 +12,18 @@ class PostsController < ApplicationController
 
   def edit
   end
+
+private 
+  def spam
+    @posts = Post.all
+    @posts.each_with_index do |post, index|
+      if (index == 0) || (index % 5 == 4)
+# Can work as well as two lines (22 & 23) or as one line (24)      
+#       post.title = "SPAM"
+#       post.save!
+        post.update_attributes(title: "SPAM")
+      end
+    end
+  end
+  
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,15 @@
 class Post < ActiveRecord::Base
     has_many :comments
+
+# If written in model instead of controller    
+#    after_save :spam
+    
+#    def spam
+#        @posts = Post.all
+#        @posts.each_with_index do |post, index|
+#          if (index == 0) || (index % 5 == 4)
+#            post.update_attributes(title: "SPAM")
+#          end
+#        end
+#    end
 end
