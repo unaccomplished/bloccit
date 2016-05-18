529c2896e21093fc378b7408e4e7da9953622cae
Jocelyn Hsu <hsu.joce@gmail.com>
2016-05-18 23:06:16 +0000

    Checkpoint 32 Rails: CRUD completed - Create & Read

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,11 +2,27 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
+  
+  def create
+    @post = Post.new
+    @post.title = params[:post][:title]
+    @post.body = params[:post][:body]
+    
+    if @post.save
+      flash[:notice] = "Post was saved successfully."
+      redirect_to @post
+    else
+      flash.now[alert] = "There was an error saving the post. Please try again."
+      render :new
+    end
+  end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def new
+    @post = Post.new
   end
 
   def edit
