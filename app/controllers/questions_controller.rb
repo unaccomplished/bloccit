fc4eeea6cf1b612d1e2596569bd2e615357acc64
Jocelyn Hsu <hsu.joce@gmail.com>
2016-05-21 03:35:37 +0000

    Assignment 33 - edit and update completed

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -27,9 +27,22 @@ class QuestionsController < ApplicationController
   end
 
   def edit
+    @question = Question.find(params[:id])
   end
 
   def update
+    @question = Question.find(params[:id])
+    @question.title = params[:question][:title]
+    @question.body = params[:question][:body]
+    @question.resolved = params[:question][:resolved]
+
+    if @question.save
+      flash[:notice] = "Question was updated successfully."
+      redirect_to @question
+    else
+      flash.now[:alert] = "There was an error saving the question. Please try again."
+      render :edit
+    end
   end
 
   def destroy
--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe QuestionsController, type: :controller do
   let(:my_question) { Question.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph, resolved: false) }
 
-  describe "GET #index" do
+  describe "GET index" do
     it "returns http success" do
       get :index
       expect(response).to have_http_status(:success)
@@ -66,19 +66,53 @@ RSpec.describe QuestionsController, type: :controller do
    end
 
 
-#  describe "GET #edit" do
-#    it "returns http success" do
-#      get :edit
-#      expect(response).to have_http_status(:success)
-#    end
-#  end
+   describe "GET edit" do
+     it "returns http success" do
+       get :edit, {id: my_question.id}
+       expect(response).to have_http_status(:success)
+     end
+     
+     it "renders the #edit view" do
+       get :edit, {id: my_question.id}
+       expect(response).to render_template :edit
+     end
+     
+     it "assigns question to be updated to @question" do
+       get :edit, {id: my_question.id}
+       
+       question_instance = assigns(:question)
+       
+       expect(question_instance.id).to eq my_question.id
+       expect(question_instance.title).to eq my_question.title
+       expect(question_instance.body).to eq my_question.body
+       expect(question_instance.resolved).to eq my_question.resolved
+     end
+   end
 
-#  describe "GET #update" do
-#    it "returns http success" do
-#      get :update
-#      expect(response).to have_http_status(:success)
-#    end
-#  end
+   describe "PUT update" do
+     it "updates question with expected attributes" do
+       new_title = RandomData.random_sentence
+       new_body = RandomData.random_paragraph
+       new_resolved = (true || false)
+       
+       put :update, id: my_question.id, question: {title: new_title, body: new_body, resolved: new_resolved}
+       
+       updated_question = assigns(:question)
+       expect(updated_question.id).to eq my_question.id
+       expect(updated_question.title).to eq new_title
+       expect(updated_question.body).to eq new_body
+       expect(updated_question.resolved).to eq new_resolved
+     end
+     
+     it "redirects to the updated question" do
+      new_title = RandomData.random_sentence
+      new_body = RandomData.random_paragraph
+      new_resolved = (true || false)
+
+      put :update, id: my_question.id, question: {title: new_title, body: new_body, resolved: new_resolved}
+      expect(response).to redirect_to my_question
+    end
+   end
 
 #  describe "GET #destroy" do
 #    it "returns http success" do
