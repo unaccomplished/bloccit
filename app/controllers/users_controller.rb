03fe9da903c9025882512e8bef1ea812a266796f
Jocelyn Hsu <hsu.joce@gmail.com>
2016-05-30 21:55:44 +0000

    Assignment 37 completed - video solution

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,8 @@ class UsersController < ApplicationController
         @user.password_confirmation = params[:user][:password_confirmation]
         
         if @user.save
-            confirm
+            flash[:notice] = "Welcome to Bloccit #{@user.name}!"
+            redirect_to root_path
         else
             flash.now[:alert] = "There was an error creating your account. Please try again."
             render :new
@@ -24,13 +25,5 @@ class UsersController < ApplicationController
         @user.email = params[:user][:email]
         @user.password = params[:user][:password]
         @user.password_confirmation = params[:user][:password_confirmation]
-        
-        if @user.save
-            flash[:notice] = "Welcome to Bloccit #{@user.name}!"
-            redirect_to root_path
-        else
-            flash.now[:alert] = "There was an error creating your account. Please try again."
-            render :new
-        end
     end
 end
