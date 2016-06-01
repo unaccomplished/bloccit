d026b51abbf96f47411c031ea6a965fccd8fc2d6
Jocelyn Hsu <hsu.joce@gmail.com>
2016-06-01 04:08:16 +0000

    Checkpoint 39 Rails: Posts and Users completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,15 @@
 require 'random_data'
 
+# Create Users
+5.times do
+    User.create!(
+        name:       RandomData.random_name,
+        email:      RandomData.random_email,
+        password:   RandomData.random_sentence
+    )
+end
+users = User.all
+
 # Create Topics
 15.times do
     Topic.create!(
@@ -12,7 +22,8 @@ topics = Topic.all
 # Create Posts
 50.times do
     Post.create!(
-        topic: topics.sample,
+        user:   users.sample,
+        topic:  topics.sample,
         title:  RandomData.random_sentence,
         body:   RandomData.random_paragraph
     )
@@ -27,7 +38,14 @@ posts = Post.all
     )
 end
 
+user = User.first
+user.update_attributes!(
+    email: 'hsu.joce@gmail.com',
+    password: 'helloworld'
+)
+
 puts "Seed finished"
+puts "#{User.count} users created"
 puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
