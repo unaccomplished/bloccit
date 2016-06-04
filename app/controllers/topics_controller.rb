d24d1cab47c65331fca04fe930c27213df760bb4
Jocelyn Hsu <hsu.joce@gmail.com>
2016-06-04 21:27:56 +0000

    Checkpoint 42 Rails: Labels completed

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -18,6 +18,7 @@ class TopicsController < ApplicationController
         @topic = Topic.new(topic_params)
         
         if @topic.save
+            @topic.labels = Label.update_labels(params[:topic][:labels])
             flash[:notice] = "Topic was saved successfully."
             redirect_to @topic
         else
@@ -36,6 +37,7 @@ class TopicsController < ApplicationController
         @topic.assign_attributes(topic_params)
         
         if @topic.save
+            @topic.labels = Label.update_labels(params[:topic][:labels])
             flash[:notice] = "Topic was updated successfully."
             redirect_to @topic
         else
