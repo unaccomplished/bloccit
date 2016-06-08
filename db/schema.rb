b549779486d05d19090a3ab1ab682d67db5df79c
Jocelyn Hsu <hsu.joce@gmail.com>
2016-06-08 21:55:40 +0000

    Reset migration and seed

--- db/schema.rb
+++ db/schema.rb
@@ -13,25 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20160606203534) do
 
-  create_table "advertisements", force: :cascade do |t|
-    t.string   "title"
-    t.text     "body"
-    t.integer  "price"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "commentings", force: :cascade do |t|
-    t.integer  "comment_id"
-    t.integer  "commentable_id"
-    t.string   "commentable_type"
-    t.datetime "created_at",       null: false
-    t.datetime "updated_at",       null: false
-  end
-
-  add_index "commentings", ["comment_id"], name: "index_commentings_on_comment_id"
-  add_index "commentings", ["commentable_type", "commentable_id"], name: "index_commentings_on_commentable_type_and_commentable_id"
-
   create_table "comments", force: :cascade do |t|
     t.text     "body"
     t.integer  "post_id"
@@ -83,35 +64,13 @@ ActiveRecord::Schema.define(version: 20160606203534) do
   add_index "posts", ["topic_id"], name: "index_posts_on_topic_id"
   add_index "posts", ["user_id"], name: "index_posts_on_user_id"
 
-  create_table "questions", force: :cascade do |t|
-    t.string   "title"
-    t.text     "body"
-    t.boolean  "resolved"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "sponsored_posts", force: :cascade do |t|
-    t.string   "title"
-    t.text     "body"
-    t.integer  "price"
-    t.integer  "topic_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  add_index "sponsored_posts", ["topic_id"], name: "index_sponsored_posts_on_topic_id"
-
   create_table "topics", force: :cascade do |t|
     t.string   "name"
     t.boolean  "public",      default: true
     t.text     "description"
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
-    t.integer  "comment_id"
   end
-
-  add_index "topics", ["comment_id"], name: "index_topics_on_comment_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
