6a160a464dcbccc7a39ee4f207324017b91041c6
Jocelyn Hsu <hsu.joce@gmail.com>
2016-06-10 00:10:06 +0000

    Assignment 42 - Work in progress troubleshooting with mentor 2

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160605205117) do
+ActiveRecord::Schema.define(version: 20160606203534) do
 
   create_table "commentings", force: :cascade do |t|
     t.integer  "comment_id"
@@ -34,6 +34,16 @@ ActiveRecord::Schema.define(version: 20160605205117) do
 
   add_index "comments", ["post_id"], name: "index_comments_on_post_id"
   add_index "comments", ["user_id"], name: "index_comments_on_user_id"
+
+  create_table "favorites", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "favorites", ["post_id"], name: "index_favorites_on_post_id"
+  add_index "favorites", ["user_id"], name: "index_favorites_on_user_id"
 
   create_table "labelings", force: :cascade do |t|
     t.integer  "label_id"
@@ -59,6 +69,7 @@ ActiveRecord::Schema.define(version: 20160605205117) do
     t.datetime "updated_at", null: false
     t.integer  "topic_id"
     t.integer  "user_id"
+    t.float    "rank"
   end
 
   add_index "posts", ["topic_id"], name: "index_posts_on_topic_id"
@@ -83,5 +94,16 @@ ActiveRecord::Schema.define(version: 20160605205117) do
     t.datetime "updated_at",      null: false
     t.integer  "role"
   end
+
+  create_table "votes", force: :cascade do |t|
+    t.integer  "value"
+    t.integer  "user_id"
+    t.integer  "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "votes", ["post_id"], name: "index_votes_on_post_id"
+  add_index "votes", ["user_id"], name: "index_votes_on_user_id"
 
 end
