aa63ef120167ec64757eb8e7c744d8527c2f92ea
Jocelyn Hsu <hsu.joce@gmail.com>
2016-06-17 00:23:35 +0000

    Assignment 46 completed - publicly & privately viewable scopes

--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -34,5 +34,17 @@ RSpec.describe Topic, type: :model do
         expect(Topic.visible_to(nil)).to eq([@public_topic])
       end
     end
+    
+    describe "publicly_viewable" do
+      it "returns a collection of public topics" do
+        expect(Topic.publicly_viewable).to eq([@public_topic])
+      end
+    end
+      
+    describe "privately_viewable" do
+      it "returns a collection of private topics" do
+        expect(Topic.privately_viewable).to eq([@private_topic])
+      end
+    end
   end
 end
